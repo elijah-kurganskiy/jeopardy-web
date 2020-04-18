13469ee53cd794e6ebd12df957e3937549e2ab7e
Ilia Kurganskiy <ilya.kurganskii@gmail.com>
2020-04-19 00:35:37 +0400

    feat(login): add routes and base login screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { API_HOST } from "./config/server";
+import Login from "./screens/Login";
+
+const client = new ApolloClient({
+  uri: `${API_HOST}/graphql`,
+  headers: {
+    "client-name": "Jeopardy [web]",
+    "client-version": "1.0.0",
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Router>
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
+    </ApolloProvider>
   );
 }
 
