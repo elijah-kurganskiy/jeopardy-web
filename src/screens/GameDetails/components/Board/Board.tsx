2a5e73d2e4f6109d170c0a2bb31059f5a10fc2da
Ilia Kurganskiy <ilya.kurganskii@gmail.com>
2020-05-02 12:55:29 +0400

    feat: update design for game

--- src/screens/GameDetails/components/Board/Board.tsx
+++ src/screens/GameDetails/components/Board/Board.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RoundType } from "types";
-import Theme from "./Theme";
+import Theme from "./components/Theme";
+import styles from "./Board.module.css";
 
 interface BoardProps {
   round: RoundType;
@@ -8,9 +9,9 @@ interface BoardProps {
 
 function Board({ round: { themes } }: BoardProps) {
   return (
-    <div>
+    <div className={styles.board}>
       {themes.map((theme) => (
-        <Theme key={theme.id} theme={theme} />
+        <Theme key={theme.id} theme={theme} className={styles.board__theme} />
       ))}
     </div>
   );
